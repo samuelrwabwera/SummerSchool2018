fba38662b367577cbe743003a7538bda6b14938d
samuelrwabwera <rwabwerakankunda@gmail.com>
2018-06-11 13:47:33 +0300

    Subtraction function

--- Add.js
+++ Add.js
@@ -7,3 +7,8 @@ function Multiplication(num1,num2){
     return(num1 * num2);
 }
 console.log(Multiplication(3,5));
+
+function Subtraction(num1,num2){
+    return(num1-num2);
+}
+console.log(Subtraction(3,6));
